5be54d8ca6020e61992496b38ab1697ce9fd464f
robert suqui <losdksodms@gmial,.com>
2024-12-11 10:45:29 -0500

    User login function

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ app.use(express.json())
 
 
 const pool = new Pool({
-
-
+    user: 'fantasyleaguedb',
+    host: 'localhost',
+    database: 'fantasyleaguedb',
+    port: 5432
 })
+
+module.exports = pool;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Fantasy League API!");
+});
+
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({ message: "Database connected!", serverTime: result.rows[0].now });
+    } catch (err) {
+        res.status(500).json({ error: "Database connection failed", details: err.message });
+    }
+});
 
 
 app.listen(5001, () => console.log('Server is running on http://localhost:5001'))
